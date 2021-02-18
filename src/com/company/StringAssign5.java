54b4fc6fdcc8eff14ee38babdd10696b66cacc42
Santa Domijana-Zviedre <balticadriatic@gmail.com>
2021-02-18 23:16:27 +0100

    Assignment 5

--- src/com/company/StringAssign5.java
+++ src/com/company/StringAssign5.java
@@ -1,45 +1,40 @@
 package com.company;
 
-import java.sql.SQLOutput;
-import java.util.Locale;
-
 public class StringAssign5 {
 
     public static void main(String[] args) {
+//        String str = "Mom";
+//        String str = "Was it a car or a cat I saw?";
+        String str = "Madam, in Eden, I'm Adam.";
+//        String str = "Yo, banana boy!";
 
-        String str = "Mom";
-        String str1 = "Was it a car or a cat I saw?";
-        String str2 = "Madam, in Eden, I'm Adam.";
-        String str3 = "Yo, banana boy!";
+        String strClean = convertString(str);
 
-        toLowerCase(str, str1, str2, str3);
-
-        if(isPalindrome(str, str1, str2, str3))
+        System.out.println(str);
+        if (isPalindrome(strClean))
             System.out.println("Yes, it is palindrome.");
         else
             System.out.println("No, it isn't palindrome.");
     }
 
-    public static boolean isPalindrome(String str, String str1, String str2, String str3) {
+    public static String convertString(String str) {
 
-        str = str.toLowerCase();
+        str = str.toLowerCase(); // how to apply Pattern.CASE_INSENSITIVE here?
+        String strClean = str.replaceAll("[^a-z]", "");
+
+        return strClean;
+    }
+
+    public static boolean isPalindrome(String strClean) {
 
         int i = 0;
-        int j = str.length() - 1;
+        int j = strClean.length() - 1;
         while (i < j) {
-            if (str.charAt(i) != str.charAt(j))
+            if (strClean.charAt(i) != strClean.charAt(j))
                 return false;
             i++;
             j--;
         }
         return true;
-    }
-
-    public static void toLowerCase(String str, String str1, String str2, String str3) {
-
-        str = str.toLowerCase();
-        str1 = str1.toLowerCase();
-        str2 = str2.toLowerCase();
-        str3 = str3.toLowerCase();
     }
 }
